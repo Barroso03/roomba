b5613991c61d74f7bee61fb23ae97064e7bbe72f
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2023-02-23 13:41:40 +0100

    cambios

--- roomba2.py
+++ roomba2.py
@@ -1,24 +1,95 @@
-zona1=(500,150)
-zona2=(480,101)
-zona3=(309,480)
-zona4=(90,220)
-zonas=[]
-zonas.append(zona1)
-zonas.append(zona2)
-zonas.append(zona3)
-zonas.append(zona4)
+from tkinter import *
 
-def superficie_a_limpiar(zonas):
-    superficie=0
-    for zona in zonas:
-        superficie+=zona[0]*zona[1]
-    superficie=superficie/10000
-    print("La superficie a limpiar es de ",superficie,"m2")
-superficie_a_limpiar(zonas)
-def tiempo_estimado(zonas):
-    tiempo=0
-    velocidad=2
-    for zona in zonas:
-        tiempo+=((zona[0]*zona[1]/10000)/velocidad)
-    print("El tiempo estimado es de ",tiempo,"segundos")
-tiempo_estimado(zonas)
+ventana = Tk()
+ventana.title("Roomba")
+ventana.geometry("1000x1200")
+botonenviar = StringVar()
+
+largobjeto = Label(ventana, text="Largo del objeto:")
+largobjeto.grid(row=0, column=0, sticky=W)
+cuadrolargobjeto = Entry(ventana)
+cuadrolargobjeto.grid(row=0, column=1)
+
+alturaobjeto = Label(ventana, text="Altura del objeto:")
+alturaobjeto.grid(row=1, column=0,sticky=W)
+cuadroalturaobjeto = Entry(ventana)
+cuadroalturaobjeto.grid(row=1, column=1)
+
+largohabitacion = Label(ventana, text="Largo de la habitación:")
+largohabitacion.grid(row=2, column=0, sticky=W)
+cuadrolargohabitacion = Entry(ventana)
+cuadrolargohabitacion.grid(row=2, column=1)
+
+alturahabitacion = Label(ventana, text="Altura de la habitación:")
+alturahabitacion.grid(row=3, column=0, sticky=W)
+cuadroalturahabitacion = Entry(ventana)
+cuadroalturahabitacion.grid(row=3, column=1)
+
+posicionx1 = Label(ventana, text="Coordenada eje x primer punto:")
+posicionx1.grid(row=4, column=0, sticky=W)
+cuadroposicionx1 = Entry(ventana)
+cuadroposicionx1.grid(row=4, column=1)
+
+posiciony1 = Label(ventana, text="Coordenada eje y primer punto:")
+posiciony1.grid(row=5, column=0, sticky=W)
+cuadroposiciony1 = Entry(ventana)
+cuadroposiciony1.grid(row=5, column=1)
+
+
+def roomba():
+    # si le das al boton enviar, se ejecuta esta funcion
+    # se obtienen los datos de los cuadros de texto
+    largobjeto = int(cuadrolargobjeto.get())
+    alturaobjeto = int(cuadroalturaobjeto.get())
+    largohabitacion = int(cuadrolargohabitacion.get())
+    alturahabitacion = int(cuadroalturahabitacion.get())
+    posicionx1 = int(cuadroposicionx1.get())
+    posiciony1 = int(cuadroposiciony1.get())
+    posicionx2 = posicionx1 + largobjeto
+    posiciony2 = posiciony1 + alturaobjeto
+    # se calcula la superficie a limpiar
+    superficie = (largohabitacion * alturahabitacion) - (largobjeto * alturaobjeto)# m^2
+    # se calcula el tiempo estimado
+    velocidad= 2#m^2/s
+    tiempo = superficie / velocidad
+    # se muestra el resultado en la ventana
+    resultado = Label(ventana, text="La superficie a limpiar es de " + str(superficie) + " m^2")
+    resultado.grid(row=4, column=0, sticky=W)
+    resultado = Label(ventana, text="El tiempo estimado es de " + str(tiempo) + " segundos")
+    resultado.grid(row=5, column=0, sticky=W)
+    # dibujamos la habitacion
+    canvas = Canvas(ventana, width=largohabitacion, height=alturahabitacion)
+    canvas.grid(row=7, column=0, sticky=W)
+    # pinta el fondo de la habitacion
+    canvas.create_rectangle(0, 0, largohabitacion, alturahabitacion, fill="white", outline="orange")
+    # dibujamos el objeto
+    canvas.create_rectangle(posicionx1, posiciony1,posicionx2,posiciony2, fill="brown", outline="orange")
+    # en el rectangulo blanco pon objeto
+    canvas.create_text(posicionx1 + largobjeto/2, posiciony1 + alturaobjeto/2, text="Objeto")
+   # haz una linea horizontal
+    canvas.create_line(0, posiciony1, largohabitacion, posiciony1, fill="orange")
+    # encima de la linea horizontal pon la altura del objeto
+    canvas.create_text(largohabitacion/2, posiciony1/2, text="Zona 1",fill="blue")
+    # haz una linea vertical
+    canvas.create_line(posicionx1, posiciony2, posicionx1, alturahabitacion, fill="orange")
+    canvas.create_text(posicionx1/2, alturahabitacion/2, text="Zona 2",fill="blue")
+    # haz una linea vertical
+    canvas.create_line(posicionx2, posiciony2,posicionx2,alturahabitacion, fill="orange")
+    canvas.create_text(posicionx2 + (largohabitacion-posicionx2)/2, alturahabitacion/2, text="Zona 3",fill="blue")
+    canvas.create_text(posicionx1+(largobjeto/2),posiciony2+(alturahabitacion-posiciony2)/2 , text="Zona 4",fill="blue")
+  
+
+
+
+botonenviar = Button(ventana, text="Enviar", command=roomba)
+botonenviar.grid(row=6, column=0, sticky=W)
+
+
+
+
+
+
+
+
+
+ventana.mainloop()
